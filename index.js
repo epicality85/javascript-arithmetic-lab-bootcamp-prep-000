3bd13aaf978b3af0f0077b3036b676d2f11b1922
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 13:30:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+// adding Function
+function add(a, b)
+{
+  return a + b
+}
+
+// subtracting function
+function subtract(a, b)
+{
+  return a - b
+}
+
+// multiplying function
+function multiply(a, b)
+{
+  return a * b
+}
+
+// dividing function
+function divide(a, b)
+{
+  return a / b
+}
+
